9d13d004505538ef9eb8d6bd46658f14e0543016
Mitko <mitkoiliev505@gmail.comm>
2023-10-16 19:08:22 +0200

    localStorage fix

--- components/note-list.tsx
+++ components/note-list.tsx
@@ -4,41 +4,41 @@ import { useNoteContext } from '@/context/note-context';
 import SingleNote from './single-note';
 
 export default function NoteList() {
-  const { notes, deleteNote } = useNoteContext();
+   const { notes, deleteNote } = useNoteContext();
 
-  function handleDeleteNote(id: number) {
-    deleteNote(id);
-  }
+   function handleDeleteNote(id: number) {
+      deleteNote(id);
+   }
 
-  return (
-    <main className='flex flex-col w-full h-full flex-1'>
-      <section className='grid grid-cols-1 gap-2'>
-        {notes.map((note: Note, index: number) => {
-          return (
-            <div key={index}>
-              <SingleNote color={note.color}>
-                <div className='flex justify-between items-center gap-4'>
-                  <p className='font-bold text-slate-800 text-xl truncate'>
-                    {note.content}
-                  </p>
-                  <span
-                    className='font-extrabold'
-                    onClick={() => handleDeleteNote(note.id)}
-                  >
-                    x
-                  </span>
-                </div>
-                <h1 className='text-slate-900 font-bold text-sm my-5'>
-                  {note.title}
-                </h1>
-              </SingleNote>
-            </div>
-          );
-        })}
-        {notes.length === 0 && (
-          <h2 className='text-white text-center text-xl'>No notes to show.</h2>
-        )}
-      </section>
-    </main>
-  );
+   return (
+      <main className='flex flex-col w-full h-full flex-1'>
+         <section className='grid grid-cols-1 gap-2'>
+            {notes.map((note: Note, index: number) => {
+               return (
+                  <div key={index}>
+                     <SingleNote color={note.color}>
+                        <div className='flex justify-between items-center gap-4'>
+                           <p className='font-bold text-slate-800 text-xl truncate'>
+                              {note.content}
+                           </p>
+                           <span
+                              className='font-extrabold'
+                              onClick={() => {
+                                 handleDeleteNote(note.id);
+                              }}
+                           >
+                              x
+                           </span>
+                        </div>
+                        <h1 className='text-slate-900 font-bold text-sm my-5'>{note.title}</h1>
+                     </SingleNote>
+                  </div>
+               );
+            })}
+            {notes.length === 0 && (
+               <h2 className='text-white text-center text-xl'>No notes to show.</h2>
+            )}
+         </section>
+      </main>
+   );
 }
--- context/note-context.tsx
+++ context/note-context.tsx
@@ -8,61 +8,50 @@ const NoteContext = createContext<NoteContextType | undefined>(undefined);
 
 // Define a custom hook to access the context
 export const useNoteContext = (): NoteContextType => {
-  const context = useContext(NoteContext);
-  if (!context) {
-    throw new Error('useNoteContext must be used within a NoteProvider');
-  }
-  return context;
+   const context = useContext(NoteContext);
+   if (!context) {
+      throw new Error('useNoteContext must be used within a NoteProvider');
+   }
+   return context;
 };
 
 // Create a NoteProvider component
-export const NoteProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
-  const [notes, setNotes] = useState<Note[]>([]);
-  const [nextId, setNextId] = useState<number>(0);
+export const NoteProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+   const [notes, setNotes] = useState<Note[]>([]);
+   const [nextId, setNextId] = useState<number>(0);
 
-  const noteColors = ['#FF5733', '#33FF57', '#3384FF', '#FF33F1', '#FFE933'];
-  const colorIndex = notes.length % noteColors.length;
+   const noteColors = ['#FF5733', '#33FF57', '#3384FF', '#FF33F1', '#FFE933'];
+   const colorIndex = notes.length % noteColors.length;
 
-  // useEffect(() => {
-  //   // Load notes from localStorage when the component mounts
-  //   const storedNotes = localStorage.getItem('notes');
-  //   if (storedNotes) {
-  //     setNotes(JSON.parse(storedNotes));
-  //     setNextId(
-  //       Math.max(...JSON.parse(storedNotes).map((note: Note) => note.id)) + 1
-  //     );
-  //   }
-  // }, []);
+   useEffect(() => {
+      // Load notes from localStorage during the initial component mount
+      const storedNotes = localStorage.getItem('notes');
+      if (storedNotes) {
+         setNotes(JSON.parse(storedNotes));
+      }
+   }, []);
 
-  // useEffect(() => {
-  //   localStorage.setItem('notes', JSON.stringify(notes));
-  // }, [notes]);
+   function addNote(content: string, title: string) {
+      const newNote: Note = {
+         id: nextId,
+         title: title,
+         content: content,
+         color: noteColors[colorIndex],
+      };
+      setNotes([...notes, newNote]);
+      setNextId((prev: number) => prev + 1);
+      localStorage.setItem('notes', JSON.stringify([...notes, newNote]));
+   }
 
-  function addNote(content: string, title: string) {
-    const newNote: Note = {
-      id: nextId,
-      title: title,
-      content: content,
-      color: noteColors[colorIndex],
-    };
-    setNotes([...notes, newNote]);
-    setNextId((prev: number) => prev + 1);
-    // localStorage.setItem('notes', JSON.stringify([...notes, newNote]));
-  }
+   function deleteNote(id: any) {
+      const updatedNotes = notes.filter((n) => n.id !== id);
+      setNotes(updatedNotes);
+      localStorage.setItem('notes', JSON.stringify(updatedNotes));
+   }
 
-  function deleteNote(id: number) {
-    const updatedNotes = notes.filter((n) => n.id !== id);
-    setNotes(updatedNotes);
-    // localStorage.setItem('notes', JSON.stringify(updatedNotes));
-  }
-
-  return (
-    <NoteContext.Provider value={{ notes, addNote, deleteNote }}>
-      {children}
-    </NoteContext.Provider>
-  );
+   return (
+      <NoteContext.Provider value={{ notes, addNote, deleteNote }}>{children}</NoteContext.Provider>
+   );
 };
 
 export default NoteContext;
